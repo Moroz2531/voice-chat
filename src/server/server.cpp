6f4286f24c38b1d3b99db027f8ab86820f3522a2
Denis-Nosov <dnosov537@gmail.com>
2026-05-07 10:42:34 +0700

    fix server

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -11,172 +11,173 @@ using namespace server;
 Server::Server(size_t id) : id_{id} {}
 
 void Server::run() {
-    if (jt_.joinable())
-        throw std::runtime_error("the server is already running!");
-    jt_ = std::jthread([this](std::stop_token stok) { runLoop(stok); });
+  if (jt_.joinable())
+    throw std::runtime_error("the server is already running!");
+  jt_ = std::jthread([this](std::stop_token stok) { runLoop(stok); });
 }
 
 void Server::stop() {
-    if (jt_.joinable()) {
-        jt_.request_stop();
-        jt_.join();
-    }
+  if (jt_.joinable()) {
+    jt_.request_stop();
+    jt_.join();
+  }
 }
 
-bool Server::joinable() const noexcept {
-    return jt_.joinable();
+bool Server::joinable() const noexcept { return jt_.joinable(); }
+
+void Server::insert(const containers::Socket &sfd) {
+  sfds_[sfd] = std::make_shared<DataUser>(sfd);
+  epoll_event ev;
+  ev.events = EPOLLIN | EPOLLOUT | EPOLLERR | EPOLLHUP | EPOLLRDHUP | EPOLLET;
+  ev.data.fd = sfd;
+  ep_.insert(sfd, ev);
 }
 
-void Server::insert(const containers::Socket& sfd) {
-    sfds_[sfd] = std::make_shared<DataUser>(sfd);
-    epoll_event ev;
-    ev.events = EPOLLIN | EPOLLOUT | EPOLLERR | EPOLLHUP | EPOLLRDHUP | EPOLLET;
-    ev.data.fd = sfd;
-    ep_.insert(sfd, ev);
+void Server::erase(const containers::Socket &sfd) {
+  if (contains(sfd)) {
+    ep_.erase(sfd);
+    sfds_.erase(sfd);
+  }
 }
 
-void Server::erase(const containers::Socket& sfd) {
-    if (contains(sfd)) {
-        ep_.erase(sfd);
-        sfds_.erase(sfd);
-    }
-}
+size_t Server::size() const { return sfds_.size(); }
 
-size_t Server::size() const {
-    return sfds_.size();
-}
+bool Server::empty() const { return sfds_.empty(); }
 
-bool Server::empty() const {
-    return sfds_.empty();
-}
-
-bool Server::contains(const containers::Socket& sfd) const {
-    return sfds_.contains(sfd);
+bool Server::contains(const containers::Socket &sfd) const {
+  return sfds_.contains(sfd);
 }
 
 void Server::runLoop(std::stop_token stok) {
-    try {
-        epoll_event evs[Options::MAX_EVENTS];
-        containers::Socket sfd;
-        std::string data;
-        std::shared_ptr<DataUser> du;
+  try {
+    epoll_event evs[Options::MAX_EVENTS];
+    containers::Socket sfd;
+    std::string data;
+    std::shared_ptr<DataUser> du;
 
-        containers::Parse p;
-        ParseInfoSV pinfo{data, du};
+    containers::Parse p;
+    ParseInfo pinfo{data, du};
 
-        data.reserve(DATA_BYTES_MAX_LEN);
-        fillParse(p, pinfo);
+    data.reserve(DATA_BYTES_MAX_LEN);
+    fillParse(p, pinfo);
 
-        auto handleInput = [&] {
-            data = sfd.recv();
-            if (data.length() >= 2) {
-                uint16_t op;
-                std::memcpy(&op, data.data(), 2);
-                p.execute(op);
-            } else
-                throw std::out_of_range("server (loop): receive incorrect message from client (-1)");
-            return 0;
-        };
+    auto handleInput = [&] {
+      data = sfd.recv();
+      if (data.length() >= 2) {
+        uint16_t op;
+        std::memcpy(&op, data.data(), 2);
+        p.execute(op);
+      } else
+        throw std::out_of_range(
+            "server (loop): receive incorrect message from client (-1)");
+      return 0;
+    };
 
-        auto handleOutput = [&] {
-            if (*du != chs_.size()) {
-                try {
-                    std::vector<char> ids;
-                    ids.reserve(2 + chs_.size() * sizeof(uint64_t));
-                    ids.push_back(0);
-                    ids.push_back(10);
-                    for (auto& [first, second] : chs_) {
-                        char* bytes = reinterpret_cast<char*>(&second);
-                        ids.insert(ids.end(), bytes, bytes + sizeof(uint64_t));
-                    }
-                    if (sfd.send(ids.data(), ids.size(), 0))
-                        return -1;
-                    *du = chs_.size();
-                } catch (const std::runtime_error& re) {
-                    throw;
-                } catch (const std::exception& e) {
-                    std::osyncstream(std::cerr) << e.what() << '\n';
-                }
-            }
-            if (du->connectChannel) {
-                in_port_t port = static_cast<VoiceChannel>(*chs_[du->channelIndex]).port();
-                char* bytes = reinterpret_cast<char*>(port);
-                char data[4] = {0, 11, bytes[0], bytes[1]};
-                if (sfd.send(data, sizeof(data), 0))
-                    return -1;
-                *du = chs_.size();
-                du->connectChannel = false;
-            }
-            return 0;
-        };
-
-        while (!stok.stop_requested()) {
-            int c = ep_.wait(evs, Options::MAX_EVENTS, Options::TIMEOUT_MS);
-            for (int i = 0; i < c; ++i) {
-                du = sfds_[evs[i].data.fd];
-                sfd = static_cast<containers::Socket>(*du);
-                try {
-                    if (evs[i].events & (EPOLLIN | EPOLLRDHUP))
-                        if (handleInput()) {
-                            erase(sfd);
-                            continue;
-                        }
-                    if (evs[i].events & EPOLLOUT)
-                        if (handleOutput()) {
-                            erase(sfd);
-                            continue;
-                        }
-                    if (evs[i].events & (EPOLLERR | EPOLLHUP | EPOLLRDHUP))
-                        erase(sfd);
-                } catch (const std::out_of_range& oor) {
-                    std::osyncstream(std::cerr) << oor.what() << '\n';
-                } catch (const std::runtime_error& re) {
-                    erase(sfd);
-                    std::osyncstream(std::cerr) << re.what() << '\n';
-                } catch (const std::exception& e) {
-                    std::osyncstream(std::cout) << e.what() << '\n';
-                }
-            }
+    auto handleOutput = [&] {
+      if (*du != chs_.size()) {
+        try {
+          std::vector<char> ids;
+          ids.reserve(2 + chs_.size() * sizeof(uint64_t));
+          ids.push_back(0);
+          ids.push_back(10);
+          for (auto &[first, second] : chs_) {
+            char *bytes = reinterpret_cast<char *>(&second);
+            ids.insert(ids.end(), bytes, bytes + sizeof(uint64_t));
+          }
+          if (sfd.send(ids.data(), ids.size(), 0))
+            return -1;
+          *du = chs_.size();
+        } catch (const std::runtime_error &re) {
+          throw;
+        } catch (const std::exception &e) {
+          std::osyncstream(std::cerr) << e.what() << '\n';
         }
-    } catch (const std::exception& e) {
-        std::osyncstream(std::cout) << e.what() << '\n';
+      }
+      if (du->procReq) {
+        in_port_t port =
+            static_cast<VoiceChannel>(*chs_[du->channelIndex]).port();
+        char *bytes = reinterpret_cast<char *>(port);
+        char data[4] = {0, 11, bytes[0], bytes[1]};
+        if (sfd.send(data, sizeof(data), 0))
+          return -1;
+        du->procReq = false;
+      }
+      return 0;
+    };
+
+    while (!stok.stop_requested()) {
+      int c = ep_.wait(evs, Options::MAX_EVENTS, Options::TIMEOUT_MS);
+      for (int i = 0; i < c; ++i) {
+        du = sfds_[evs[i].data.fd];
+        sfd = static_cast<containers::Socket>(*du);
+        try {
+          if (evs[i].events & (EPOLLIN | EPOLLRDHUP))
+            if (handleInput()) {
+              erase(sfd);
+              continue;
+            }
+          if (evs[i].events & EPOLLOUT)
+            if (handleOutput()) {
+              erase(sfd);
+              continue;
+            }
+          if (evs[i].events & (EPOLLERR | EPOLLHUP | EPOLLRDHUP))
+            erase(sfd);
+        } catch (const std::out_of_range &oor) {
+          std::osyncstream(std::cerr) << oor.what() << '\n';
+        } catch (const std::runtime_error &re) {
+          erase(sfd);
+          std::osyncstream(std::cerr) << re.what() << '\n';
+        } catch (const std::exception &e) {
+          std::osyncstream(std::cout) << e.what() << '\n';
+        }
+      }
     }
+  } catch (const std::exception &e) {
+    std::osyncstream(std::cout) << e.what() << '\n';
+  }
 }
 
-void Server::fillParse(containers::Parse& p, ParseInfoSV& pinfo) {
-    p.insert(5, [&] {
-        if (pinfo.data.length() != 17)
-            throw std::out_of_range("server (loop): receive incorrect message from client (5)");
-        uint64_t chId;
-        in_addr_t ip;
-        in_port_t port;
-        char act;
-        std::memcpy(&chId, pinfo.data.data() + 2, 8);
-        std::memcpy(&ip, pinfo.data.data() + 10, 4);
-        std::memcpy(&port, pinfo.data.data() + 14, 2);
-        std::memcpy(&act, pinfo.data.data() + 16, 1);
-        auto ch = static_cast<VoiceChannel>(*chs_[chId]);
-        if (act) {
-            ch.insert(ip, port);
-            if (!ch.joinable())
-                ch.run();
-            pinfo.du->connectChannel = true;
-            pinfo.du->channelIndex = chId;
-        } else {
-            ch.erase(ip, port);
-            if (ch.empty())
-                ch.stop();
-        }
-    });
-    p.insert(7, [&] {
-        chs_.insert(std::pair<size_t, std::unique_ptr<Channel>>{chIdx_, std::make_unique<VoiceChannel>(chIdx_)});
-        ++chIdx_;
-    });
-    p.insert(8, [&] {
-        if (pinfo.data.length() != 10)
-            throw std::out_of_range("server (loop): receive incorrect message from client (8)");
-        uint64_t chId;
-        std::memcpy(&chId, pinfo.data.data() + 2, 8);
-        chs_.erase(chId);
-    });
+void Server::fillParse(containers::Parse &p, ParseInfo &pinfo) {
+  p.insert(5, [&] {
+    if (pinfo.data.length() != 17)
+      throw std::out_of_range(
+          "server (loop): receive incorrect message from client (5)");
+    uint64_t chId;
+    in_addr_t ip = pinfo.du->sfd.ip<true>();
+    in_port_t port;
+    char act;
+    std::memcpy(&chId, pinfo.data.data() + 2, 8);
+    std::memcpy(&port, pinfo.data.data() + 10, 2);
+    std::memcpy(&act, pinfo.data.data() + 12, 1);
+    auto ch = static_cast<VoiceChannel>(*chs_[chId]);
+    if (act) {
+      if (ch.contains(ip, port))
+        return;
+      ch.insert(ip, port);
+      if (!ch.joinable())
+        ch.run();
+      pinfo.du->channelIndex = chId;
+      pinfo.du->procReq = true;
+      pinfo.du->connectChannel = true;
+    } else {
+      ch.erase(ip, port);
+      pinfo.du->connectChannel = false;
+      if (ch.empty())
+        ch.stop();
+    }
+  });
+  p.insert(7, [&] {
+    chs_.insert(std::pair<size_t, std::unique_ptr<Channel>>{
+        chIdx_, std::make_unique<VoiceChannel>(chIdx_)});
+    ++chIdx_;
+  });
+  p.insert(8, [&] {
+    if (pinfo.data.length() != 10)
+      throw std::out_of_range(
+          "server (loop): receive incorrect message from client (8)");
+    uint64_t chId;
+    std::memcpy(&chId, pinfo.data.data() + 2, 8);
+    chs_.erase(chId);
+  });
 }
--- src/server/server.hpp
+++ src/server/server.hpp
@@ -5,73 +5,72 @@
 #include <thread>
 #include <unordered_map>
 
-#include <general/parse.hpp>
 #include "channel.hpp"
+#include "general/parse.hpp"
 #include "lib/epoll.hpp"
 #include "lib/socket.hpp"
-#include "mainserver.hpp"
 
 namespace server {
 struct DataUser {
-    containers::Socket sfd;
-    uint64_t chsSize{0};
-    uint64_t channelIndex{0};
-    bool connectChannel{false};
+  containers::Socket sfd;
+  uint64_t chsSize{0};
+  uint64_t channelIndex{0};
+  bool connectChannel{false}, procReq{false};
 
-    void operator=(const containers::Socket& fd) { sfd = fd; }
-    void operator=(size_t size) noexcept { chsSize = size; }
+  void operator=(const containers::Socket &fd) { sfd = fd; }
+  void operator=(size_t size) noexcept { chsSize = size; }
 
-    explicit operator containers::Socket() { return sfd; }
-    operator size_t() const noexcept { return chsSize; }
+  explicit operator containers::Socket() { return sfd; }
+  operator size_t() const noexcept { return chsSize; }
 };
 
-struct ParseInfoSV {
-    std::string_view data;
-    std::shared_ptr<DataUser>& du;
+struct ParseInfo {
+  std::string_view data;
+  std::shared_ptr<DataUser> &du;
 };
 
 class Server {
-    enum Options {
-        MAX_EVENTS = 50,
-        TIMEOUT_MS = 1,
-    };
+  enum Options {
+    MAX_EVENTS = 50,
+    TIMEOUT_MS = 1,
+  };
 
-   public:
-    Server(size_t id);
-    Server(const Server& other) = delete;
-    Server(Server&& other) noexcept = delete;
-    ~Server() { stop(); }
+public:
+  Server(size_t id);
+  Server(const Server &other) = delete;
+  Server(Server &&other) noexcept = delete;
+  ~Server() { stop(); }
 
-    Server& operator=(const Server& rhs) = delete;
-    Server& operator=(Server&& rhs) noexcept = delete;
+  Server &operator=(const Server &rhs) = delete;
+  Server &operator=(Server &&rhs) noexcept = delete;
 
-   public:
-    operator uint64_t() const noexcept { return id_; }
+public:
+  operator uint64_t() const noexcept { return id_; }
 
-   public:
-    void run();
-    void stop();
-    bool joinable() const noexcept;
+public:
+  void run();
+  void stop();
+  bool joinable() const noexcept;
 
-   public:
-    void insert(const containers::Socket& sfd);
-    void erase(const containers::Socket& sfd);
+public:
+  void insert(const containers::Socket &sfd);
+  void erase(const containers::Socket &sfd);
 
-    size_t size() const;
-    bool empty() const;
-    bool contains(const containers::Socket& sfd) const;
+  size_t size() const;
+  bool empty() const;
+  bool contains(const containers::Socket &sfd) const;
 
-   private:
-    void runLoop(std::stop_token stok);
-    void fillParse(containers::Parse& p, ParseInfoSV& pinfo);
+private:
+  void runLoop(std::stop_token stok);
+  void fillParse(containers::Parse &p, ParseInfo &pinfo);
 
-   private:
-    uint64_t id_;
-    std::jthread jt_;
-    containers::Epoll ep_;
-    uint64_t chIdx_{0};
-    std::unordered_map<uint64_t, std::unique_ptr<Channel>> chs_;
-    std::unordered_map<int, std::shared_ptr<DataUser>> sfds_;
+private:
+  size_t id_;
+  std::jthread jt_;
+  containers::Epoll ep_;
+  uint64_t chIdx_{0};
+  std::unordered_map<uint64_t, std::unique_ptr<Channel>> chs_;
+  std::unordered_map<int, std::shared_ptr<DataUser>> sfds_;
 };
 
-}  // namespace server
+} // namespace server
