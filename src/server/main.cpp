aa5913075db28d57ae50d1fe098b444b23e635e6
Denis-Nosov <dnosov537@gmail.com>
2026-05-07 10:44:11 +0700

    add main for server

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,11 +1,3 @@
 #include "server.hpp"
 
-#include <chrono>
-
-using namespace std::chrono_literals;
-
-int main() {
-    server::Server sv{0};
-    sv.run();
-    std::this_thread::sleep_for(5s);
-}
+int main() { server::Server sv{0}; }
